3e8b2ce4acad3a1fe71da7fd4eafc17b51604103
Joseph <joseph.dena@student.moringaschool.com>
2023-10-22 17:01:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // code your solution here
+function saturdayFun(activity ="roller-skate"){
+    console.log(activity);
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+saturdayFun();
+
+mondayWork();
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+
+
+// function wrapAdjective(value ='*'){
+//     if (value = '*'){
+//         return (function (adjective = "a hard worker") {
+//             return (`You are ${value}${adjective}${value}!`);
+//            });
+//     }else if (value = "||"){
+//         return (function (adjective = "a dedicated programmer") {
+//             return (`You are ${value}${adjective}${value}!`);
+//           });
+
+//     }
+
+// }
+// wrapAdjective()();
+
+
+function wrapAdjective(obj = "*"){
+    if (obj === "||") {
+        return function (adjective = "a dedicated programmer"){ return `You are ${obj}${adjective}${obj}!`}; 
+    }
+    else {
+        return function (adjective = "a hard worker"){ return `You are ${obj}${adjective}${obj}!`};
+    }
+  };
+
+  wrapAdjective();
